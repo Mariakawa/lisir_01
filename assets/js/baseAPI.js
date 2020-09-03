db5837b40dad53b6451047053c652a6d441d6b40
Mariakawaii <1069081760@qq.com>
2020-09-03 14:47:37 +0800

    index over

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,47 @@
 // 发送请求之前执行
 // options:请求参数对象
+// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
+// 会先调用 ajaxPrefilter 这个函数
+// 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function (options) {
+    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+    // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载 complete 回调函数
+    options.complete = function (res) {
+        // console.log('执行了 complete 回调：')
+        // console.log(res)
+        // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1. 强制清空 token
+            localStorage.removeItem('token')
+            // 2. 强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
+
+// if (options.url.indexOf('/my/') !== -1) {
+//     options.headers = {
+//         // 统一为有权限的接口,设置headers请求头
+//         Authorization: localStorage.getItem('token') || ''
+//     }
+// }
+
+// // 当请求结束后,判断用户的设置访问权限
+// options.complete = function (res) {
+//     // 当用户身份认证失败后
+//     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败!') {
+//         // 1.强制清空本地 token  
+//         localStorage.removeItem('token')
+//         // 2.强制跳转到登录页面
+//         location.href = '/login.html'
+//     }
+// }
